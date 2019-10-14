d369e27d553cc1e511f0fb9ac02c0a7d49509544
Bekarys <kusbek1994@gmail.com>
2019-10-14 16:35:56 +0600

    created signup page, created users and signed in as superuser into admin

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.account, name = 'account'),
-    path('signup', views.signup, name = 'signup'),
-    path('login', views.login, name = 'login')
+    path('signup/', views.signup, name = 'signup'),
+    path('login/', views.login, name = 'login'),
+    path('logout/', views.logout, name = 'logout'),
 ]
