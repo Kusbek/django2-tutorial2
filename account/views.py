505963733f0387a7d08f746e0c9753ccd6e4a86f
Bekarys <kusbek1994@gmail.com>
2019-10-14 12:50:34 +0600

    added account app into directory, and corresponding pages

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def account(request):
+    return render(request, 'account/account.html')
+
+def signup(request):
+    return render(request, 'account/account.html')
+def login(request):
+    return render(request, 'account/account.html')
--- tutorial2/urls.py
+++ tutorial2/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 import product.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', product.views.homepage, name = 'homepage'),
+    path('account/', include('account.urls')),
 ]
