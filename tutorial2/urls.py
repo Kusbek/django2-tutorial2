65c760397f4a6685209bbfe4ee08d599a6ec0bb5
Bekarys <kusbek1994@gmail.com>
2019-10-15 15:25:07 +0600

    added create.html and its features, media save option

--- tutorial2/urls.py
+++ tutorial2/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 import product.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', product.views.homepage, name = 'homepage'),
     path('account/', include('account.urls')),
-]
+    path('product/', include('product.urls')),
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
